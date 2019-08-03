80efd2f7ba0577c2eced656d65559f0499690bcf
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2019-08-03 18:18:53 +0500

    Getting started with React and frontend

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </>
+    </BrowserRouter>
+  );
+};
+
+export default App;
